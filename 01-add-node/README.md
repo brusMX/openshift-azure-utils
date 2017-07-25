6f7b5ee657ae32407a782d4cade16b1fa480b7ad
Bruno <elbuen@brus.mx>
2017-07-25 14:37:55 -0700

    Adding a Dockerfile that has the add-node python script

--- 01-add-node/README.md
+++ 01-add-node/README.md
@@ -4,4 +4,7 @@ This util allows you to scale your cluster up. You need to provide the character
 
 ## Proposed things to work on
 
-1. Create a docker image to 
+1. Create script to set up VM with network configuration
+1. Create script to call ansible script to add Node to cluster
+1. Set up a dockerfile ready to use the libraries and tools for python on Azure
+1. Publish docker image to deploy as a pod inside the Openshift that can manage the new pool of nodes.
---
+++ 01-add-node/add-node.py
@@ -0,0 +1,197 @@
+"""Deploy a VM
+
+Needed env vars:
+
+AZURE_TENANT_ID: your Azure Active Directory tenant id or domain
+AZURE_CLIENT_ID: your Azure Active Directory Application Client ID
+AZURE_CLIENT_SECRET: your Azure Active Directory Application Secret
+AZURE_SUBSCRIPTION_ID: your Azure Subscription Id
+"""
+import os
+import traceback
+
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import DiskCreateOption
+
+from msrestazure.azure_exceptions import CloudError
+
+from haikunator import Haikunator
+
+
+haikunator = Haikunator()
+
+# Azure Datacenter
+LOCATION = 'westus'
+
+# Resource Group
+GROUP_NAME = 'azure-sample-group-virtual-machines'
+
+# Network
+VNET_NAME = 'azure-sample-vnet'
+SUBNET_NAME = 'azure-sample-subnet'
+
+# VM
+OS_DISK_NAME = 'azure-sample-osdisk'
+STORAGE_ACCOUNT_NAME = haikunator.haikunate(delimiter='')
+
+IP_CONFIG_NAME = 'azure-sample-ip-config'
+NIC_NAME = 'azure-sample-nic'
+USERNAME = 'azureusername'
+PASSWORD = 'Pa$$w0rd9111'
+VM_NAME = 'RHEL001'
+
+VM_REFERENCE = {
+    'linux': {
+        'publisher': 'RedHat',
+        'offer': 'RHEL',
+        'sku': '7.3',
+        'version': 'latest'
+    },
+    'windows': {
+        'publisher': 'MicrosoftWindowsServerEssentials',
+        'offer': 'WindowsServerEssentials',
+        'sku': 'WindowsServerEssentials',
+        'version': 'latest'
+    }
+}
+
+
+def get_credentials():
+    subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+    credentials = ServicePrincipalCredentials(
+        client_id=os.environ['AZURE_CLIENT_ID'],
+        secret=os.environ['AZURE_CLIENT_SECRET'],
+        tenant=os.environ['AZURE_TENANT_ID']
+    )
+    return credentials, subscription_id
+
+    
+
+
+def add_node():
+    """Create resource and VM """
+    #
+    # Create all clients with an Application (service principal) token provider
+    #
+    credentials, subscription_id = get_credentials()
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    compute_client = ComputeManagementClient(credentials, subscription_id)
+    network_client = NetworkManagementClient(credentials, subscription_id)
+
+    ###########
+    # Prepare #
+    ###########
+
+    # Create Resource group
+    print('\nCreate Resource Group')
+    resource_client.resource_groups.create_or_update(GROUP_NAME, {'location': LOCATION})
+
+    try:
+        # Create a NIC
+        nic = create_nic(network_client)
+
+        #############
+        # VM Sample #
+        #############
+
+        # Create Linux VM
+        print('\nCreating Linux Virtual Machine')
+        vm_parameters = create_vm_parameters(nic.id, VM_REFERENCE['linux'])
+        async_vm_creation = compute_client.virtual_machines.create_or_update(
+            GROUP_NAME, VM_NAME, vm_parameters)
+        async_vm_creation.wait()
+
+        # List VM in resource group
+        print('\nList VMs in resource group')
+        for vm in compute_client.virtual_machines.list(GROUP_NAME):
+            print("\tVM: {}".format(vm.name))
+
+    except CloudError:
+        print('add-node python script has failed:', traceback.format_exc(), sep='\n')
+    else:
+        print('All example operations completed successfully!')
+    finally:
+        # Delete Resource group and everything in it
+        print('\nDelete Resource Group')
+
+
+
+def create_nic(network_client):
+    """Create a Network Interface for a VM.
+    """
+    # Create VNet
+    print('\nCreate Vnet')
+    async_vnet_creation = network_client.virtual_networks.create_or_update(
+        GROUP_NAME,
+        VNET_NAME,
+        {
+            'location': LOCATION,
+            'address_space': {
+                'address_prefixes': ['10.0.0.0/16']
+            }
+        }
+    )
+    async_vnet_creation.wait()
+
+    # Create Subnet
+    print('\nCreate Subnet')
+    async_subnet_creation = network_client.subnets.create_or_update(
+        GROUP_NAME,
+        VNET_NAME,
+        SUBNET_NAME,
+        {'address_prefix': '10.0.0.0/24'}
+    )
+    subnet_info = async_subnet_creation.result()
+
+    # Create NIC
+    print('\nCreate NIC')
+    async_nic_creation = network_client.network_interfaces.create_or_update(
+        GROUP_NAME,
+        NIC_NAME,
+        {
+            'location': LOCATION,
+            'ip_configurations': [{
+                'name': IP_CONFIG_NAME,
+                'subnet': {
+                    'id': subnet_info.id
+                }
+            }]
+        }
+    )
+    return async_nic_creation.result()
+
+
+def create_vm_parameters(nic_id, vm_reference):
+    """Create the VM parameters structure.
+    """
+    return {
+        'location': LOCATION,
+        'os_profile': {
+            'computer_name': VM_NAME,
+            'admin_username': USERNAME,
+            'admin_password': PASSWORD
+        },
+        'hardware_profile': {
+            'vm_size': 'Standard_DS1_v2'
+        },
+        'storage_profile': {
+            'image_reference': {
+                'publisher': vm_reference['publisher'],
+                'offer': vm_reference['offer'],
+                'sku': vm_reference['sku'],
+                'version': vm_reference['version']
+            },
+        },
+        'network_profile': {
+            'network_interfaces': [{
+                'id': nic_id,
+            }]
+        },
+    }
+
+
+if __name__ == "__main__":
+    add_node()
